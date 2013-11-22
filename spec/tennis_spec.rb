67914dd24e6418ab94ada03c7091f7125161eeb2
MakerSquare Student <students@themakersquare.com>
2013-11-22 13:10:38 -0600

    I have a working player class and tests but I have written my first Game .score test and it is failing

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,6 +1,9 @@
+require 'pry'
 require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
+
+
 require_relative '../tennis'
 
 describe Tennis::Game do
@@ -12,7 +15,11 @@ describe Tennis::Game do
       expect(game.player2).to be_a(Tennis::Player)
     end
 
-    it 'sets the opponent for each player'
+    it 'sets the opponent for each player' do
+   
+      expect(game.player1.opponent).to eq (game.player2)
+      expect(game.player2.opponent).to eq (game.player1)
+    end
   end
 
   describe '#wins_ball' do
@@ -22,12 +29,24 @@ describe Tennis::Game do
       expect(game.player1.points).to eq(1)
     end
   end
+
+  describe '.score' do
+    context 'a player wins forty to love' do
+      it 'displays game: Player1 Bill' do
+      
+        @player1.points = 4
+        @player2.points = 0
+
+        expect(game.game_score).to eq ('game: Player1 Bill')
+      end
+    end
+  end
 end
 
 describe Tennis::Player do
   let(:player) do
-    player = Tennis::Player.new
-    player.opponent = Tennis::Player.new
+    player = Tennis::Player.new("Player1 Bill")
+    player.opponent = Tennis::Player.new("Player2 Ted")
 
     return player
   end
@@ -46,6 +65,8 @@ describe Tennis::Player do
     end
   end
 
+
+
   describe '#score' do
     context 'when points is 0' do
       it 'returns love' do
@@ -62,11 +83,65 @@ describe Tennis::Player do
     end
     
     context 'when points is 2' do
-      it 'returns thirty'  
+      it 'returns thirty' do
+        player.points = 2
+
+        expect(player.score).to eq ('thirty') 
+      end
     end
     
     context 'when points is 3' do
-      it 'returns forty' 
+      it 'returns forty' do
+        player.points = 3
+        player.opponent.points = 1
+
+        expect(player.score).to eq ('forty')
+      end
     end
+
+    context 'When the players are tied at deuce' do
+      it 'returns deuce' do
+        player.points = 3
+        player.opponent.points = 3
+
+        expect(player.score).to eq ('deuce')
+      end
+    end
+
+    context 'When one player has advantage' do
+      it 'returns advantage to player1' do
+        player.points = 5
+        player.opponent.points = 4
+
+        expect(player.score).to eq ('advantage: Player1 Bill')
+      end
+
+      it 'returns advantage to player2' do
+
+        player.points = 7
+        player.opponent.points = 8
+
+        expect(player.score).to eq ('advantage: Player2 Ted')
+      end
+    end
+
+    context 'When one player wins the game' do
+      it 'returns game: Player1 Bill' do
+
+        player.points = 10
+        player.opponent.points = 8
+
+        expect(player.score).to eq ('game: Player1 Bill')
+
+      end
+
+      it 'returns game: Player2 Ted' do
+
+        player.points = 6
+        player.opponent.points = 8
+
+        expect(player.score).to eq ('game: Player2 Ted')
+      end
+    end    
   end
 end
--- tennis.rb
+++ tennis.rb
@@ -1,10 +1,10 @@
 module Tennis
   class Game
-    attr_accessor :player1, :player2
+    attr_accessor :player1, :player2, :score
 
     def initialize
-      @player1 = Player.new
-      @player2 = Player.new
+      @player1 = Player.new("Player1 Bill")
+      @player2 = Player.new("Player2 Ted")
 
       @player1.opponent = @player2
       @player2.opponent = @player1
@@ -16,28 +16,80 @@ module Tennis
     #
     # Returns the score of the winning player. 
     def wins_ball(winner)
-      # TODO: Think it's gross to pass an integer instead of a player object?
-      # Then reimplement this method!
+      if winner == 1
+        winning_player = @player1
+      else
+        winning_player = @player2
+      end
+
+      winning_player.record_won_ball!
+    end
+
+
+# This method cycles through scoring: first if a player wins, second
+# if a player has advantange, third if score is deuce, fourth if score is tied.
+# and finally if one player is ahead of the other.
+    
+
+    def game_score
+    
+      if (@player1.points > 3 || @player2.points > 3) && (@player2.points - @player1.points > 1 || @player1.points - @player2.points > 1)
+        puts "#{@player1.score}"
+      elsif (@player1.points >= 3 || @player2.points >= 3) && (@player2.points - @player1.points == 1 || @player1.points - @player2.points == 1)
+        puts "#{@player1.score}"
+      elsif @player1.points == 3 && @player2.points == 3
+        puts "#{@player1.score}"
+      elsif @player1.points == @player2.points && @player1.points < 3 
+        puts "#{@player1.score} all"
+      elsif @player1.points != @player2.points && @player1.points <= 3 && @player2.points <= 3 
+        puts "#{@player1.score}: #{@player2.score}"
+      else
+        puts "this is an unfamiliar scoring situation"
+      end
     end
   end
 
-  class Player
-    attr_accessor :points, :opponent
+  
 
-    def initialize
+  class Player
+    attr_accessor :points, :opponent, :name
+
+    def initialize(name) 
       @points = 0
+      @name = name
     end
+
 
     # Increments the score by 1.
     #
     # Returns the integer new score.
     def record_won_ball!
       @points += 1
+      return @points
     end
 
+
+
     # Returns the String score for the player.
+    # This handles deuce advantage and winning the game
     def score
       return 'love' if @points == 0
+      return 'fifteen' if @points == 1
+      return 'thirty' if @points == 2
+      if @points == 3 && (self.opponent.points < 3)
+        return 'forty' 
+      elsif @points - self.opponent.points == 0 
+        return 'deuce'
+      elsif @points - self.opponent.points == 1
+        return "advantage: #{self.name}"
+      elsif self.opponent.points - @points == 1
+        return "advantage: #{self.opponent.name}"
+      elsif @points - self.opponent.points == 2
+        return "game: #{self.name}"
+      else 
+        return "game: #{self.opponent.name}"
+      end
+
     end
   end
 end
